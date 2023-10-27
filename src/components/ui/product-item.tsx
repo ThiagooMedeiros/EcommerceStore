76261719f60e915cbe528c796ce6413030a03995
Thiago <thiagoodemedeiros@gmail.com>
2023-10-27 19:53:26 -0300

    fix: ensure product prices are wrapped in one line

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -2,15 +2,14 @@ import { ProductWithTotalPrice } from "@/helpers/product";
 import Image from "next/image";
 import { Badge } from "./badge";
 import { ArrowDownIcon } from "lucide-react";
-
 interface ProductItemProps {
   product: ProductWithTotalPrice;
 }
 
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
-    <div className="flex max-w-[156px] flex-col gap-4">
-      <div className="relative flex h-[170px] w-[156px] items-center justify-center rounded-lg bg-accent">
+    <div className="flex max-w-[170px] flex-col gap-4">
+      <div className="relative flex h-[170px] w-[170px] items-center justify-center rounded-lg bg-accent">
         <Image
           src={product.imageUrls[0]}
           height={0}
@@ -22,32 +21,30 @@ const ProductItem = ({ product }: ProductItemProps) => {
           }}
           alt={product.name}
         />
-
         {product.discountPercentage > 0 && (
           <Badge className="absolute left-3 top-3 px-2 py-[2px] ">
             <ArrowDownIcon size={14} /> {product.discountPercentage}%
           </Badge>
         )}
       </div>
-
       <div className="flex flex-col gap-1">
         <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm">
           {product.name}
         </p>
 
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 ">
           {product.discountPercentage > 0 ? (
             <>
-              <p className="font-semibold">
+              <p className="overflow-hidden text-ellipsis whitespace-nowrap font-semibold">
                 R$ {product.totalPrice.toFixed(2)}
               </p>
 
-              <p className="text-xs line-through opacity-75">
+              <p className="overflow-hidden text-ellipsis whitespace-nowrap text-xs line-through opacity-75">
                 R$ {Number(product.basePrice).toFixed(2)}
               </p>
             </>
           ) : (
-            <p className="text-sm font-semibold">
+            <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm font-semibold">
               R$ {product.basePrice.toFixed(2)}
             </p>
           )}
@@ -56,5 +53,4 @@ const ProductItem = ({ product }: ProductItemProps) => {
     </div>
   );
 };
-
 export default ProductItem;
