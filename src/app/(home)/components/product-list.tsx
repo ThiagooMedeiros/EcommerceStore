c20da9f22e2122872b07457fa8a391e1963e3c8e
Thiago <thiagoodemedeiros@gmail.com>
2023-10-27 21:11:24 -0300

    feat: add page to show category products

--- src/app/(home)/components/product-list.tsx
+++ src/app/(home)/components/product-list.tsx
@@ -1,22 +1,18 @@
 import ProductItem from "@/components/ui/product-item";
 import { computeProductTotalPrice } from "@/helpers/product";
 import { Product } from "@prisma/client";
-
 interface ProductListProps {
   products: Product[];
 }
-
 const ProductList = ({ products }: ProductListProps) => {
   return (
     <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
       {products.map((product) => (
-        <ProductItem
-          key={product.id}
-          product={computeProductTotalPrice(product)}
-        />
+        <div key={product.id} className="w-[170px] max-w-[170px]">
+          <ProductItem product={computeProductTotalPrice(product)} />
+        </div>
       ))}
     </div>
   );
 };
-
 export default ProductList;
--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -8,8 +8,8 @@ interface ProductItemProps {
 
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
-    <div className="flex max-w-[170px] flex-col gap-4">
-      <div className="relative flex h-[170px] w-[170px] items-center justify-center rounded-lg bg-accent">
+    <div className="flex flex-col gap-4">
+      <div className="relative flex h-[170px] w-full items-center justify-center rounded-lg bg-accent">
         <Image
           src={product.imageUrls[0]}
           height={0}
@@ -31,14 +31,12 @@ const ProductItem = ({ product }: ProductItemProps) => {
         <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm">
           {product.name}
         </p>
-
         <div className="flex items-center gap-2 ">
           {product.discountPercentage > 0 ? (
             <>
               <p className="overflow-hidden text-ellipsis whitespace-nowrap font-semibold">
                 R$ {product.totalPrice.toFixed(2)}
               </p>
-
               <p className="overflow-hidden text-ellipsis whitespace-nowrap text-xs line-through opacity-75">
                 R$ {Number(product.basePrice).toFixed(2)}
               </p>
