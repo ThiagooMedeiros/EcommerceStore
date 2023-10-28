51b521b5ee32723e3d40e9e4315dc70aec8f261a
Thiago <thiagoodemedeiros@gmail.com>
2023-10-27 21:21:18 -0300

    feat: ensure user goes to home page on logo click

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -22,7 +22,6 @@ import { Avatar, AvatarImage } from "./avatar";
 import { AvatarFallback } from "@radix-ui/react-avatar";
 import { Separator } from "./separator";
 import Link from "next/link";
-
 const Header = () => {
   const { status, data } = useSession();
   const handleLoginClick = async () => {
@@ -89,8 +88,7 @@ const Header = () => {
               <PercentIcon size={16} />
               Ofertas
             </Button>
-
-            <SheetClose asChild> 
+            <SheetClose asChild>
               <Link href="/catalog">
                 <Button
                   variant="outline"
@@ -104,9 +102,13 @@ const Header = () => {
           </div>
         </SheetContent>
       </Sheet>
-      <h1 className="text-lg font-semibold">
-        <span className="text-primary">Ecommerce</span> Store
-      </h1>
+
+      <Link href="/">
+        <h1 className="text-lg font-semibold">
+          <span className="text-primary">Ecommerce</span> Store
+        </h1>
+      </Link>
+
       <Button size="icon" variant="outline">
         <ShoppingCartIcon />
       </Button>
