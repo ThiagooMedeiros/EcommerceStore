f98115079002586d52adbd17a3bd30e355896e08
Thiago <thiagoodemedeiros@gmail.com>
2023-10-29 19:26:04 -0300

    feat: ensure product quantity can be modified on cart

--- src/components/ui/cart-item.tsx
+++ src/components/ui/cart-item.tsx
@@ -1,18 +1,24 @@
-import { CartProduct } from "@/providers/cart";
+import { CartContext, CartProduct } from "@/providers/cart";
 import Image from "next/image";
 import { Button } from "./button";
 import { ArrowLeftIcon, ArrowRightIcon, TrashIcon } from "lucide-react";
+import { useContext } from "react";
 
 interface CartItemProps {
   product: CartProduct;
 }
 
 const CartItem = ({ product }: CartItemProps) => {
+  const { decreaseProductQuantity } = useContext(CartContext);
+
+  const handleDecreaseProductQuantityClick = () => {
+    decreaseProductQuantity(product.id);
+  };
+
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center gap-4">
         {/* PARTE DIREITA (FOTO E NOME) */}
-
         <div className="flex h-[77px] w-[77px] items-center justify-center rounded-lg bg-accent">
           <Image
             src={product.imageUrls[0]}
@@ -23,10 +29,8 @@ const CartItem = ({ product }: CartItemProps) => {
             className="h-auto max-h-[70%] w-auto max-w-[80%]"
           />
         </div>
-
         <div className="flex flex-col">
           <p className="text-xs">{product.name}</p>
-
           <div className="flex items-center gap-2">
             <p className="text-sm font-bold">
               R$ {product.totalPrice.toFixed(2)}
@@ -39,24 +43,26 @@ const CartItem = ({ product }: CartItemProps) => {
           </div>
 
           <div className="flex items-center gap-1">
-            <Button size="icon" variant="outline" className="h-8 w-8">
+            <Button
+              size="icon"
+              variant="outline"
+              className="h-8 w-8"
+              onClick={handleDecreaseProductQuantityClick}
+            >
               <ArrowLeftIcon size={16} />
             </Button>
 
             <span className="text-xs">{product.quantity}</span>
-
             <Button size="icon" variant="outline" className="h-8 w-8">
               <ArrowRightIcon size={16} />
             </Button>
           </div>
         </div>
       </div>
-
       <Button size="icon" variant="outline">
         <TrashIcon size={16} />
       </Button>
     </div>
   );
 };
-
 export default CartItem;
--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -1,35 +1,34 @@
 "use client";
-
 import { ProductWithTotalPrice } from "@/helpers/product";
 import { ReactNode, createContext, useState } from "react";
-
 export interface CartProduct extends ProductWithTotalPrice {
   quantity: number;
 }
-
 interface ICartContext {
   products: CartProduct[];
   cartTotalPrice: number;
   cartBasePrice: number;
   cartTotalDiscount: number;
   addProductToCart: (product: CartProduct) => void;
+  decreaseProductQuantity: (productId: string) => void;
 }
+
 export const CartContext = createContext<ICartContext>({
   products: [],
   cartTotalPrice: 0,
   cartBasePrice: 0,
   cartTotalDiscount: 0,
   addProductToCart: () => {},
+  decreaseProductQuantity: () => {},
 });
+
 const CartProvider = ({ children }: { children: ReactNode }) => {
   const [products, setProducts] = useState<CartProduct[]>([]);
-
   const addProductToCart = (product: CartProduct) => {
     // se o produto já estiver no carrinho, apenas aumente a sua quantidade
     const productIsAlreadyOnCart = products.some(
       (cartProduct) => cartProduct.id === product.id,
     );
-
     if (productIsAlreadyOnCart) {
       setProducts((prev) =>
         prev.map((cartProduct) => {
@@ -39,16 +38,30 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
               quantity: cartProduct.quantity + product.quantity,
             };
           }
-
           return cartProduct;
         }),
       );
-
       return;
     }
-
     // se não, adicione o produto à lista
     setProducts((prev) => [...prev, product]);
+  };
+
+  const decreaseProductQuantity = (productId: string) => {
+    setProducts((prev) =>
+      prev
+        .map((cartProduct) => {
+          if (cartProduct.id === productId) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity - 1,
+            };
+          }
+
+          return cartProduct;
+        })
+        .filter((cartProduct) => cartProduct.quantity > 0),
+    );
   };
 
   return (
@@ -56,6 +69,7 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
       value={{
         products,
         addProductToCart,
+        decreaseProductQuantity,
         cartTotalPrice: 0,
         cartBasePrice: 0,
         cartTotalDiscount: 0,
