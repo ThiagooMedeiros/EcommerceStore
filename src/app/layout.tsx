61b5a796ad58983dd504f17f2cb67c435953af02
Thiago <thiagoodemedeiros@gmail.com>
2023-10-29 16:07:57 -0300

    feat: add CartContext

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import Header from '@/components/ui/header'
 import { AuthProvider } from "@/providers/auth";
 import Footer from "@/components/ui/footer";
+import CartProvider from "@/providers/cart";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,9 +24,11 @@ export default function RootLayout({
         
       <div className="flex h-full flex-col">
           <AuthProvider>
-            <Header />
-              <div className="flex-1">{children}</div>
-            <Footer />
+            <CartProvider>
+              <Header />
+                <div className="flex-1">{children}</div>
+              <Footer />
+            </CartProvider>
           </AuthProvider>
         </div>
 
