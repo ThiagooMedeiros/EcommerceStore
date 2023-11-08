56fcd2ced9d3c7a664dc6e28fe7c8364c201dc11
Thiago <thiagoodemedeiros@gmail.com>
2023-11-08 16:27:56 -0300

    feat: ensure footer is responsive on desktop

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,9 +1,10 @@
 const Footer = () => {
   return (
-    <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75">
-      © 2023 Copyright <span className="font-semibold">Ecommerce Store</span>
+    <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75 lg:text-sm">
+      <div className="mx-auto lg:container">
+        © 2023 Copyright <span className="font-semibold">FSW Store</span>
+      </div>
     </div>
   );
 };
-
 export default Footer;
