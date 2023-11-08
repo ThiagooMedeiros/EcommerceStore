5a382ac273cd4ee15eea777ad6fdf2cac801fb62
Thiago <thiagoodemedeiros@gmail.com>
2023-11-08 17:28:41 -0300

    style: footer

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75 lg:text-sm">
+    <div className="bg-card px-8 py-4 text-[0.625rem] opacity-75 lg:text-sm">
       <div className="mx-auto lg:container">
-        © 2023 Copyright <span className="font-semibold">FSW Store</span>
+        © 2023 Copyright <span className="font-semibold">Ecommerce Store</span>
       </div>
     </div>
   );
