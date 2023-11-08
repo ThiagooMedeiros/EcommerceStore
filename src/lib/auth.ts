e153770cf32254dd9a9f7ef432a8109d117e177f
Thiago <thiagoodemedeiros@gmail.com>
2023-11-08 08:56:37 -0300

    feat: add createOrder server action

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,6 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import GoogleProvider from "next-auth/providers/google";
 import { AuthOptions } from "next-auth";
 import { prismaClient } from "./prisma";
-
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prismaClient),
   providers: [
@@ -11,4 +10,15 @@ export const authOptions: AuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async session({ session, token, user }) {
+      session.user = { ...session.user, id: user.id } as {
+        id: string;
+        name: string;
+        email: string;
+      };
+
+      return session;
+    },
+  },
 };
