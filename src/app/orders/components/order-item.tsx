80eb0a4e1eac7d43ccdf871995433430cc43bdd1
Thiago <thiagoodemedeiros@gmail.com>
2023-11-08 10:38:46 -0300

    feat: add totals to order item

--- src/app/orders/components/order-item.tsx
+++ src/app/orders/components/order-item.tsx
@@ -8,6 +8,9 @@ import { Card } from "@/components/ui/card";
 import { Order, Prisma } from "@prisma/client";
 import { format } from "date-fns";
 import OrderProductItem from "./order-product-item";
+import { Separator } from "@/components/ui/separator";
+import { useMemo } from "react";
+import { computeProductTotalPrice } from "@/helpers/product";
 
 interface OrderItemProps {
   order: Prisma.OrderGetPayload<{
@@ -18,14 +21,36 @@ interface OrderItemProps {
     };
   }>;
 }
+
 const OrderItem = ({ order }: OrderItemProps) => {
+  const subtotal = useMemo(() => {
+    return order.orderProducts.reduce((acc, orderProduct) => {
+      return (
+        acc + Number(orderProduct.product.basePrice) * orderProduct.quantity
+      );
+    }, 0);
+  }, [order.orderProducts]);
+
+  const total = useMemo(() => {
+    return order.orderProducts.reduce((acc, product) => {
+      const productWithTotalPrice = computeProductTotalPrice(product.product);
+
+      return acc + productWithTotalPrice.totalPrice * product.quantity;
+    }, 0);
+  }, [order.orderProducts]);
+
+  const totalDiscounts = subtotal - total;
+
   return (
     <Card className="px-5">
       <Accordion type="single" className="w-full" collapsible>
         <AccordionItem value={order.id}>
           <AccordionTrigger>
             <div className="flex flex-col gap-1 text-left">
-              Pedido com {order.orderProducts.length} produto(s)
+              <p>Pedido com {order.orderProducts.length} produto(s)</p>
+              <span className="text-sm opacity-60">
+                Feito em {format(order.createdAt, "d/MM/y 'às' HH:mm")}
+              </span>
             </div>
           </AccordionTrigger>
 
@@ -47,13 +72,42 @@ const OrderItem = ({ order }: OrderItemProps) => {
                   <p className="opacity-60">Cartão</p>
                 </div>
               </div>
-
               {order.orderProducts.map((orderProduct) => (
                 <OrderProductItem
                   key={orderProduct.id}
                   orderProduct={orderProduct}
                 />
               ))}
+
+              <div className="flex w-full flex-col gap-1 text-xs">
+                <Separator />
+
+                <div className="flex w-full justify-between py-3">
+                  <p>Subtotal</p>
+                  <p>R$ {subtotal.toFixed(2)}</p>
+                </div>
+
+                <Separator />
+
+                <div className="flex w-full justify-between py-3">
+                  <p>Entrega</p>
+                  <p>GRÁTIS</p>
+                </div>
+
+                <Separator />
+
+                <div className="flex w-full justify-between py-3">
+                  <p>Descontos</p>
+                  <p>-R$ {totalDiscounts.toFixed(2)}</p>
+                </div>
+
+                <Separator />
+
+                <div className="flex w-full justify-between py-3 text-sm font-bold">
+                  <p>Total</p>
+                  <p>R$ {total.toFixed(2)}</p>
+                </div>
+              </div>
             </div>
           </AccordionContent>
         </AccordionItem>
--- src/app/orders/components/order-product-item.tsx
+++ src/app/orders/components/order-product-item.tsx
@@ -1,7 +1,6 @@
 import { computeProductTotalPrice } from "@/helpers/product";
 import { OrderProduct, Prisma } from "@prisma/client";
 import Image from "next/image";
-
 interface OrderProductItemProps {
   orderProduct: Prisma.OrderProductGetPayload<{
     include: {
@@ -9,13 +8,12 @@ interface OrderProductItemProps {
     };
   }>;
 }
-
 const OrderProductItem = ({ orderProduct }: OrderProductItemProps) => {
   const productWithTotalPrice = computeProductTotalPrice(orderProduct.product);
 
   return (
     <div className="flex items-center gap-4">
-      <div className="flex h-[77px] w-[77px] items-center justify-center rounded-lg bg-accent">
+      <div className="flex h-[77px] w-[100px] items-center justify-center rounded-lg bg-accent">
         <Image
           src={orderProduct.product.imageUrls[0]}
           width={0}
@@ -25,34 +23,28 @@ const OrderProductItem = ({ orderProduct }: OrderProductItemProps) => {
           alt={orderProduct.product.name}
         />
       </div>
-
       <div className="flex w-full flex-col gap-1">
         <div className="flex w-fit rounded-md bg-accent px-3 py-1">
           <p className="text-[10px]">
-            Vendido e entregue por <span className="font-bold">Ecommerce Store</span>
+            Vendido e entregue por <span className="font-bold">FSW Store</span>
           </p>
         </div>
-
         <p className="text-xs">{orderProduct.product.name}</p>
-
         <div className="flex w-full items-center justify-between gap-1">
           <div className="flex items-center gap-1">
             <p className="text-sm font-bold">
               R$ {productWithTotalPrice.totalPrice.toFixed(2)}
             </p>
-
             {productWithTotalPrice.discountPercentage > 0 && (
               <p className="text-xs line-through opacity-60">
                 R$ {Number(productWithTotalPrice.basePrice).toFixed(2)}
               </p>
             )}
           </div>
-
           <p className="text-xs opacity-60">Qntd: {orderProduct.quantity}</p>
         </div>
       </div>
     </div>
   );
 };
-
 export default OrderProductItem;
