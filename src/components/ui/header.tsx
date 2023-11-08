d47a287c7acfd2f1e2a8350b9f743db333de5d4d
Thiago <thiagoodemedeiros@gmail.com>
2023-11-08 10:11:08 -0300

    feat: ensure orders are shown on orders page

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -5,6 +5,7 @@ import {
   LogInIcon,
   LogOutIcon,
   MenuIcon,
+  PackageSearchIcon,
   PercentIcon,
   ShoppingCartIcon,
 } from "lucide-react";
@@ -91,6 +92,18 @@ const Header = () => {
                 >
                   <HomeIcon size={16} />
                   Início
+                </Button>
+              </Link>
+            </SheetClose>
+
+            <SheetClose asChild>
+              <Link href="/orders">
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <PackageSearchIcon size={16} />
+                  Meus Pedidos
                 </Button>
               </Link>
             </SheetClose>
