5fed23c2690d8ad4a1818b312c5fd7cc42a519cc
Thiago <thiagoodemedeiros@gmail.com>
2023-11-08 10:57:41 -0300

    fix: dynamic orders page

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -4,6 +4,9 @@ import { prismaClient } from "@/lib/prisma";
 import { PackageSearchIcon } from "lucide-react";
 import { getServerSession } from "next-auth";
 import OrderItem from "./components/order-item";
+
+export const dynamic = "force-dynamic";
+
 async function OrderPage() {
   const user = getServerSession(authOptions);
   if (!user) {
